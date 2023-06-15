56badf43630acfdd89a643c3e77a85ac63fa47f8
beboopr <linnets-router.09@icloud.com>
2023-06-15 16:17:54 -0400

    Section: | Insight w insight text, Working!

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,8 +1,37 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { insights } from '../constants';
+import { staggerContainer } from '../utils/motion';
+import { TypingText, InsightCard, TitleText } from '../components';
+
 const Insights = () => (
-  <section>
-    Insights section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText
+        title="| Insights"
+        textStyles="text-center"
+      />
+      <TitleText
+        title="Insights about metaverse"
+        textStyles="text-center"
+      />
+      <div className="mt-[50px] flex flex-col gap-[30px]">
+        {insights.map((insight, index) => (
+          <InsightCard key={`insight-${index}`}
+            {...insight} index={index + 1}
+          />
+        ))}
+      </div>
+    </motion.div>
   </section>
 );
 
